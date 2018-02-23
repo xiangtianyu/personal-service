ee4a499a68fb22ceb507e7f7dc9d1af4425563ff
xiangtianyu <2233919407@qq.com>
2018-02-23 14:38:03 +0800

    pom

--- personalService/src/main/java/ty/xiang/xty/controller/BlogController.java
+++ personalService/src/main/java/ty/xiang/xty/controller/BlogController.java
@@ -1,4 +1,10 @@
 package ty.xiang.xty.controller;
 
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
 public class BlogController {
 }
--- personalService/src/main/java/ty/xiang/xty/service/BlogService.java
+++ personalService/src/main/java/ty/xiang/xty/service/BlogService.java
@@ -1,5 +1,9 @@
 package ty.xiang.xty.service;
 
+
+import org.springframework.stereotype.Service;
+
+@Service
 public class BlogService {
 
 }
